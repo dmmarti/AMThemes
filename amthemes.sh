1589bee0351ee085be326cc8381aa34e26715376
dmmarti <appletvsub2014@gmail.com>
2020-05-12 12:08:23 -0500

    addnew new themes to download and a new view sample for each

--- amthemes.sh
+++ amthemes.sh
@@ -13,6 +13,12 @@
 # then be able to easily download new themes for use.
 #
 
+# Welcome
+ dialog --backtitle "Attract Mode" --title "Attract Mode Themes Downloader Menu" \
+    --yesno "\nAttract Mode Themes Downloader Menu.\n\nThis utility will provide an easy way to download and update Attract Mode themes (layouts).\n\nSome themes have two different variants.\n\nthemename_menu = used for Main Menu Display and nested displays (Arcades, Consoles, etc)\n\nthemename_systems = used for individual system displays (Atari 2600, Atari 7800, etc)\n\nYou can easily change a displays theme by pressing\n\nTAB > Displays > displayname > Layout\n\nThen cycle through the available layouts to use.\n\n\nDo you want to proceed?" \
+    25 100 2>&1 > /dev/tty \
+    || exit
+
 function depends_amthemes() {
     if isPlatform "x11"; then
         getDepends feh
@@ -43,115 +49,43 @@ function uninstall_theme_amthemes() {
     fi
 }
 
-function changetheme() {
-# Welcome
- dialog --backtitle "Attract Mode" --title "Attract Mode Theme Change Utility Menu" \
-    --yesno "\nAttract Mode Theme Change Utility menu.\n\nThis utility will provide a quick way to change the layout(theme) for displays within Attract Mode.\n\nAfter making the chagnesu will need to restart in order for the changes to take affect.\n\nDo you want to proceed?" \
-    20 80 2>&1 > /dev/tty \
-    || exit
-
-cat /home/pi/.attract/attract.cfg |grep layout |grep -v "menu_" |grep -v param |sort -u |awk '{print $2}' > /tmp/current
-ls /home/pi/.attract/layouts > /tmp/layouts
-ls /opt/retropie/supplementary/attractmode/share/attract/layouts >> /tmp/layouts
-echo "Attrac-Man" >> /tmp/layouts
-echo "Basic" >> /tmp/layouts
-echo "Cools" >> /tmp/layouts
-echo "Orbit" >> /tmp/layouts
-echo "Reflect" >> /tmp/layouts
-echo "Verticools" >> /tmp/layouts
-
-
-let i=0 # define counting variable
-W=() # define working array
-while read -r line; do # process file by file
-    let i=$i+1
-    W+=($i "$line")
-done < <(cat /tmp/current)
-
-CURTHEME=$(dialog --title "Attract Mode Theme Change Utility" --menu "Currently used layouts - choose the one to change." 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3)
-
-clear
-
-if [ -z $CURTHEME ]; then
-   return
-else
-  clear
-  let i=0 # define counting variable
-  W=() # define working array
-  while read -r line; do # process file by file
-      let i=$i+1
-      W+=($i "$line")
-  done < <(cat /tmp/layouts)
-  currenttheme=`sed -n ${CURTHEME}p /tmp/current`
-  NEWTHEME=$(dialog --title "Attract Mode Theme Change Utility" --menu "Chose the replacement theme for ${currenttheme}." 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3)
-
-  if [ -z $NEWTHEME ]; then
-    return
-  else
-    newtheme=`sed -n ${NEWTHEME}p /tmp/layouts`
-    #echo "Going to replace ${currenttheme} with ${newtheme}"
-
-    cp /home/pi/.attract/attract.cfg /home/pi/.attract/attract.cfg.bkp
-    cp /home/pi/.attract/attract.cfg /tmp/temp_attract.cfg
-    rm /tmp/temp.cfg  2> /dev/null
-
-    while read line
-    do
-    if [[ $line == "display"* && $line != "displays_menu"* ]]; then
-      echo $line >> /tmp/temp.cfg
-    elif [[ $line == *"menu_layout"*"${currenttheme}" ]]; then
-      echo -e "\tmenu_layout               ${newtheme}" >> /tmp/temp.cfg
-    elif [[ $line == *"layout"*"${currenttheme}" && $line != "menu_layout"* ]]; then
-      echo -e "\tlayout               ${newtheme}" >> /tmp/temp.cfg
-    elif [[ $line == "rule"* ]]; then
-      echo -e "\t\t${line}" >> /tmp/temp.cfg
-    elif [[ $line == "sound" ]]; then
-      echo $line >> /tmp/temp.cfg
-    elif [[ $line == "input_map" ]]; then
-      echo $line >> /tmp/temp.cfg
-    elif [[ $line == "general" ]]; then
-      echo $line >> /tmp/temp.cfg
-    elif [[ $line == "saver_config" ]]; then
-      echo $line >> /tmp/temp.cfg
-    elif [[ $line == "layout_config"* ]]; then
-      echo $line >> /tmp/temp.cfg
-    elif [[ $line == "intro_config" ]]; then
-      echo $line >> /tmp/temp.cfg
-    elif [[ $line == "#"* ]]; then
-      echo $line >> /tmp/temp.cfg
-    else
-      echo -e "\t${line}" >> /tmp/temp.cfg
-    fi
-    done < /tmp/temp_attract.cfg
-  fi
-  
-  rm /tmp/temp_attract.cfg
-  mv /tmp/temp.cfg /home/pi/.attract/attract.cfg
-
-fi
-
-#rm /tmp/current
-#rm /tmp/layouts
-}
-
 function gui_amthemes() {
     local themes=(
-        'dmmarti robospin'
-        'dmmarti hurstyblue_system'
-        'dmmarti hurstyblue_main'
-        'dmmarti hurstyblue_nds'
-        'dmmarti cosmo-arcade'
-        'dmmarti cosmo-mainmenu'
-        'dmmarti cosmo-nds'
-        'dmmarti cosmo-systems'
-        'dmmarti grid_menu'
-        'dmmarti grid_systems'
+        'dmmarti unified_menu'
+        'dmmarti unified_systems'
+        'dmmarti unified_systems_overview'
+        'dmmarti 80s_arcades'
+        'dmmarti 80s_menu'
+        'dmmarti 80s_systems'
+        'dmmarti arcade1up_menu'
+        'dmmarti arcade1up_systems'
+        'dmmarti arcadeops'
+        'dmmarti arcaderefried'
+        'dmmarti beyond_basics_menu'
+        'dmmarti beyond_basics_systems'
+        'dmmarti bigcade'
+        'dmmarti comiccrazytoo_menu'
+        'dmmarti comiccrazytoo_systems'
+        'dmmarti cosmicrise_menu'
+        'dmmarti cosmicrise_systems'
+        'dmmarti graffiti_menu'
+        'dmmarti graffiti_systems'
+        'dmmarti hurstyblue_menu'
+        'dmmarti hurstyblue_systems'
+        'dmmarti hyperflash'
+        'dmmarti hyperflash54'
+        'dmmarti hyper_menu'
+        'dmmarti hyper_systems'
+        'dmmarti mvsops'
+        'dmmarti neon_menu'
+        'dmmarti neon_systems'
         'dmmarti silky_menu'
         'dmmarti silky_systems'
-        'dmmarti back2basics'
-        'RetroHursty69 comiccrazy'
-        'RetroHursty69 comiccrazy_menu'
-        'RetroHursty69 unifiedsnazzy'
+        'dmmarti smooth'
+        'dmmarti space_deck_menu'
+        'dmmarti space_deck_systems'
+        'dmmarti stirling_menu'
+        'dmmarti stirling_systems'
     )
     while true; do
         local theme
@@ -162,7 +96,7 @@ function gui_amthemes() {
         local default
 
         options+=(U "Update install script - script will exit when updated")
-        options+=(C "Change theme - restart Attract Mode afterwards")
+        options+=(V "View theme samples")
 
         local i=1
         for theme in "${themes[@]}"; do
@@ -191,8 +125,8 @@ function gui_amthemes() {
                 chmod 777 "amthemes.sh" 
                 exit
                 ;;
-            C)  #change theme
-                changetheme
+            V)  #view theme samples
+                view_styles
                 ;;
             *)  #install or update themes
                 theme=(${themes[choice-1]})
@@ -218,5 +152,107 @@ function gui_amthemes() {
         esac
     done
 }
+
+function view_styles() {
+
+  if [[ -f "unified_theme.png" ]]; then
+
+    local choice
+
+    while true; do
+        choice=$(dialog --backtitle "Attract Mode Themes" --title " View Samples " \
+            --ok-label OK --cancel-label Exit \
+            --menu "Chose an option to see the theme sample (10 second timeout)" 25 75 20 \
+            1 "Unified theme" \
+            2 "80s theme" \
+            3 "Arcade1Up 5:4 theme" \
+            4 "ArcadeOPS theme" \
+            5 "Arcade Refried theme" \
+            6 "Beyond Basics theme" \
+            7 "BigCade theme" \
+            8 "Comic Crazy Too theme" \
+            9 "Cosmic Rise theme" \
+            10 "Graffiti theme" \
+            11 "HurstyBlue theme" \
+            12 "HyperFlash theme" \
+            13 "Hyper theme" \
+            14 "MVSOPS theme" \
+            15 "Neon theme" \
+            16 "Silky theme" \
+            17 "Smooth theme" \
+            18 "Space Deck theme" \
+            19 "Stirling theme" \
+            2>&1 > /dev/tty)
+
+        case "$choice" in
+            1) show_theme unified_theme  ;;
+            2) show_theme 80s_theme  ;;
+            3) show_theme arcade1up_theme  ;;
+            4) show_theme arcadeops_theme  ;;
+            5) show_theme arcaderefried_theme  ;;
+            6) show_theme beyondbasics_theme  ;;
+            7) show_theme bigcade_theme  ;;
+            8) show_theme comiccrazytoo_theme  ;;
+            9) show_theme cosmicrise_theme  ;;
+            10) show_theme graffiti_theme  ;;
+            11) show_theme hurstyblue_theme  ;;
+            12) show_theme hyperflash_theme  ;;
+            13) show_theme hyper_theme  ;;
+            14) show_theme mvsops_theme  ;;
+            15) show_theme neon_theme  ;;
+            16) show_theme silky_theme  ;;
+            17) show_theme smooth_theme  ;;
+            18) show_theme spacedeck_theme  ;;
+            19) show_theme stirling_theme  ;;
+            *)  break ;;
+        esac
+    done
+
+  else
+
+    local choice
+
+    while true; do
+        choice=$(dialog --backtitle "Attract Mode Themes" --title " View Samples " \
+            --ok-label OK --cancel-label Exit \
+            --menu "Download theme samples " 25 75 20 \
+            1 "Download theme samples" \
+            2>&1 > /dev/tty)
+
+        case "$choice" in
+            1) download_themesamples  ;;
+            *)  break ;;
+        esac
+    done
+  fi
+}
+
+function show_theme() {
+local themesample="$1"
+fbi --timeout 10 --once "$themesample.png"
+}
+
+function download_themesamples() {
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/80s_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/arcade1up_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/arcadeops_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/arcaderefried_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/beyondbasics_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/bigcade_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/comiccrazytoo_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/cosmicrise_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/graffiti_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/hurstyblue_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/hyperflash_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/hyper_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/mvsops_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/neon_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/silky_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/smooth_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/spacedeck_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/stirling_theme.png"
+wget "https://raw.githubusercontent.com/dmmarti/AMThemes/master/unifiedtheme.png"
+}
+
 
 gui_amthemes
